5f3c54df3e548513522c428b74bd62fd5a2c2e64
asutoshvariar <asutoshvariar@duck.com>
2022-03-14 22:29:40 +0400

    Revert "Update config.js"
    This reverts commit cb58de97d64c791acabb6fd3af73b7f78f466385.

--- config.js
+++ config.js
@@ -71,33 +71,150 @@ const CONFIG = {
       icon: 'glasses',
       link: 'https://reddit.com',
     },
-  ],
-
-  secondButtonsContainer: [
     {
-      id: '1',
+      id: '6',
       name: 'YouTube',
       icon: 'youtube',
       link: 'https://www.youtube.com/',
     },
     {
-      id: '2',
+      id: '7',
       name: 'Spotify',
       icon: 'headphones',
       link: 'https://open.spotify.com',
     },
     {
-      id: '3',
+      id: '8',
       name: 'Twitter',
       icon: 'twitter',
       link: 'https://twitter.com/',
     },
     {
-      id: '4',
+      id: '9',
       name: 'Discord',
       icon: 'bot',
       link: 'https://discord.com/app',
     },
   ],
 
- 
+  secondButtonsContainer: [
+    {
+      id: '1',
+      name: 'Spotify',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '2',
+      name: 'Twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '3',
+      name: 'Discord',
+      icon: 'bot',
+      link: 'https://discord.com/app',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
+  firstlistsContainer: [
+    {
+      icon: 'music',
+      id: '1',
+      links: [
+        {
+          name: 'Inspirational',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Classic',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Oldies',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          name: 'Rock',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+      ],
+    },
+    {
+      icon: 'coffee',
+      id: '2',
+      links: [
+        {
+          name: 'Linkedin',
+          link: 'https://www.linkedin.com',
+        },
+        {
+          name: 'Dribbble',
+          link: 'https://www.dribbble.com',
+        },
+        {
+          name: 'Trello',
+          link: 'https://www.trello.com',
+        },
+        {
+          name: 'Slack',
+          link: 'https://www.slack.com',
+        },
+      ],
+    },
+  ],
+
+  // Second Links Container
+  secondListsContainer: [
+    {
+      icon: 'binary',
+      id: '1',
+      links: [
+        {
+          name: 'Spotify',
+          link: 'https://www.spotify.com',
+        },
+        {
+          name: 'Reddit',
+          link: 'https://www.reddit.com',
+        },
+        {
+          name: 'Hashnode',
+          link: 'https://www.hashnode.com',
+        },
+        {
+          name: 'Pocket',
+          link: 'https://www.pocket.com',
+        },
+      ],
+    },
+    {
+      icon: 'github',
+      id: '2',
+      links: [
+        {
+          name: 'Front',
+          link: 'https://www.reddit.com/r/Frontend/',
+        },
+        {
+          name: 'Rust',
+          link: 'https://www.reddit.com/r/rust/',
+        },
+        {
+          name: 'Go',
+          link: 'https://www.reddit.com/r/golang/',
+        },
+        {
+          name: 'Repos',
+          link: 'https://github.com/asutoshvariar',
+        },
+      ],
+    },
+  ],
+};
