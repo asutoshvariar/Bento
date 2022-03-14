cd8de5cb3d01af538bf5e3383967782e83f4c59b
Asutosh Variar <96536388+asutoshvariar@users.noreply.github.com>
2022-03-14 22:33:03 +0400

    Update config.js

--- config.js
+++ config.js
@@ -100,121 +100,26 @@ const CONFIG = {
   secondButtonsContainer: [
     {
       id: '1',
+      name: 'YouTube',
+      icon: 'youtube',
+      link: 'https://www.youtube.com/',
+    },
+    {
+      id: '2',
       name: 'Spotify',
       icon: 'headphones',
       link: 'https://open.spotify.com',
     },
     {
-      id: '2',
+      id: '3',
       name: 'Twitter',
       icon: 'twitter',
       link: 'https://twitter.com/',
     },
     {
-      id: '3',
+      id: '4',
       name: 'Discord',
       icon: 'bot',
       link: 'https://discord.com/app',
     },
   ],
-
-  // ┬  ┬┌─┐┌┬┐┌─┐
-  // │  │└─┐ │ └─┐
-  // ┴─┘┴└─┘ ┴ └─┘
-
-  // First Links Container
-  firstlistsContainer: [
-    {
-      icon: 'music',
-      id: '1',
-      links: [
-        {
-          name: 'Inspirational',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        },
-        {
-          name: 'Classic',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        },
-        {
-          name: 'Oldies',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        },
-        {
-          name: 'Rock',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        },
-      ],
-    },
-    {
-      icon: 'coffee',
-      id: '2',
-      links: [
-        {
-          name: 'Linkedin',
-          link: 'https://www.linkedin.com',
-        },
-        {
-          name: 'Dribbble',
-          link: 'https://www.dribbble.com',
-        },
-        {
-          name: 'Trello',
-          link: 'https://www.trello.com',
-        },
-        {
-          name: 'Slack',
-          link: 'https://www.slack.com',
-        },
-      ],
-    },
-  ],
-
-  // Second Links Container
-  secondListsContainer: [
-    {
-      icon: 'binary',
-      id: '1',
-      links: [
-        {
-          name: 'Spotify',
-          link: 'https://www.spotify.com',
-        },
-        {
-          name: 'Reddit',
-          link: 'https://www.reddit.com',
-        },
-        {
-          name: 'Hashnode',
-          link: 'https://www.hashnode.com',
-        },
-        {
-          name: 'Pocket',
-          link: 'https://www.pocket.com',
-        },
-      ],
-    },
-    {
-      icon: 'github',
-      id: '2',
-      links: [
-        {
-          name: 'Front',
-          link: 'https://www.reddit.com/r/Frontend/',
-        },
-        {
-          name: 'Rust',
-          link: 'https://www.reddit.com/r/rust/',
-        },
-        {
-          name: 'Go',
-          link: 'https://www.reddit.com/r/golang/',
-        },
-        {
-          name: 'Repos',
-          link: 'https://github.com/asutoshvariar',
-        },
-      ],
-    },
-  ],
-};
