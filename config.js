7b981d9241ccbc2cb20dac44c1d8ec75123ae1bc
Asutosh Variar <96536388+asutoshvariar@users.noreply.github.com>
2022-03-14 22:18:55 +0400

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
   // Autochange
-  autoChangeTheme: true,
+  autoChangeTheme: false,
 
   // Autochange by OS
   changeThemeByOS: true,
@@ -76,6 +76,24 @@ const CONFIG = {
       name: 'YouTube',
       icon: 'youtube',
       link: 'https://www.youtube.com/',
+    },
+    {
+      id: '7',
+      name: 'Spotify',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '8',
+      name: 'Twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '9',
+      name: 'Discord',
+      icon: 'bot',
+      link: 'https://discord.com/app',
     },
   ],
 
